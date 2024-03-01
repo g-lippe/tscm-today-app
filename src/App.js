40ea50ef25f7cf074cb27d2e7266156cca46529d
Gabriel Felippe <gabrielalexfelippe@gmail.com>
2024-03-01 12:04:51 -0300

    Match Figma

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { useChuckNorris } from './hooks/useChuckNorris';
 import { useTasksCompleted } from './hooks/useTasksCompleted';
 
 
-
 function App() {
   const data_joke = useChuckNorris();
   const { tasks_completed, isLoading, isError } = useTasksCompleted();
@@ -13,6 +12,7 @@ function App() {
 
   return (
     <div className="App">
+
       <div className='title'>
         <h1>To<span>day</span></h1>
         <p>Wake up , go ahead , do the thing not tomorrow, do <strong>to</strong>day.</p>
@@ -25,28 +25,29 @@ function App() {
         </div>
 
         <div className='cont_right'>
-
           <div className='finished'>
             <h3> Finished tasks quantity </h3>
             <h1>{String(tasks_completed).padStart(2, '0')}</h1>
           </div>
 
           <TaskForm />
-
         </div>
-
       </div>
-
-
-
+    
       {data_joke.isLoading && <h1>Loading...</h1>}
+
       {data_joke.isError && <pre>{JSON.stringify(data_joke.error)}</pre>}
+
       {data_joke &&
         <div className='cont_joke'>
           <p>"{data_joke}"</p>
           <span><strong>By api.chucknorris.io</strong></span>
         </div>
       }
+
+      <div className='cont_signature'>
+        <h5>@Made with ❤ by g-lippe</h5>
+      </div>
 
     </div>
   );
